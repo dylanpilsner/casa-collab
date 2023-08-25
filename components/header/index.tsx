c080d5844431627c6227ae88f51fad293d81b26d
Dylan <dylan.pilsner@gmail.com>
2023-08-25 20:12:19 -0300

    fix de importación a NavMenu

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,11 +1,4 @@
-import { HeaderContainer, StyledHeader } from "./styled";
-import { Logo } from "@/ui/logo";
-import { Burger } from "../burger";
-import { useEffect, useState } from "react";
-import { NavMenu } from "../nav-menu/mobile";
-import { MainButton, SecondaryButton } from "@/ui/buttons";
-import { DesktopNavMenu } from "../nav-menu/desktop";
-import { useRouter } from "next/router";
+import { StyledHeader } from "./styled";
 import { useHeader } from "@/lib/hooks";
 import { LoggedInHeader, LoggedOutHeader } from "./headers";
 
